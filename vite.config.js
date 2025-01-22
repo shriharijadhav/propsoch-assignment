1854201d10ef9f0fa715d5492d1962b9a258c153
shrihariatpepsales <shriharijadhav@pepsales.co>
2025-01-22 16:38:04 +0530

    IMP: vite config updated

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'/',
   server: {
     host: '0.0.0.0' // This allows Vite to be accessible on the network
   }
